c928a9157c713ff747bb705f186ab7efc35de9c1
Egemen <kecandir@gmail.com>
2019-10-24 22:44:42 +0300

    Python Capstone Notebook

---
+++ Capstone Project Notebook.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# This notebook will be mainly used for IBM Capstone Project
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
